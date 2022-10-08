630219712cd30112e1031d501d29e4795ce5a355
RaminSafarliCode <raminas@code.edu.az>
2022-10-08 10:51:15 +0400

    api-intro

---
+++ BigOn.WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigOn.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("from get");
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok("from post");
+        }
+
+        [HttpPut]
+        public IActionResult Edit()
+        {
+            return Ok("from put");
+        }
+
+        [HttpDelete]
+        public IActionResult Remove()
+        {
+            return Ok("from delete");
+        }
+    }
+}
